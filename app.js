dd4d6274ee3c6df4df7d1572ca6d182cb65f53b1
ravindub <ravindubandara075@gmail.com>
2023-09-04 22:57:52 +0300

    version 01 final commit

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var _ = require("lodash");
+
+let posts = [];
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -18,8 +21,20 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.get("/posts/:title", function (req, res) {
+  const reqestedTitle = _.lowerCase(req.params.title);
+
+  posts.forEach(function (post) {
+    var postTitle = _.lowerCase(post.postTitle);
+
+    if (postTitle === reqestedTitle) {
+      res.render("post", { title: post.postTitle, postBody: post.postBody });
+    }
+  });
+});
+
 app.get("/", function (req, res) {
-  res.render("home", { homeContent: homeStartingContent });
+  res.render("home", { homeContent: homeStartingContent, allPosts: posts });
 });
 
 app.get("/about", function (req, res) {
@@ -40,7 +55,9 @@ app.post("/compose", function (req, res) {
     postBody: req.body.postContent,
   };
 
-  console.log(post);
+  posts.push(post);
+
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
